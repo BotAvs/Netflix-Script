2d857c9efc563bf87df27ed36e72d1d670e2a8f0
BotAvs <137691516+BotAvs@users.noreply.github.com>
2023-12-24 03:16:02 +0100

    Testing

--- Main Code/Log In.ps1
+++ Main Code/Log In.ps1
@@ -1,39 +1,43 @@
 # Abrir el ficherin
-$email, $password = Get-Content "cuentas.txt" -Split ":"
+$cuentas = Get-Content "cuentas.txt"
 
 # Bucle para probar las cuentas
 for ($i = 0; $i -lt $cuentas.Count; $i++) {
 
-    # Obtener datos
-    $email = $cuentas[$i][0]
-    $password = $cuentas[$i][1]
+    #Bucle para sacar las lineas de cada cuenta
+    foreach ($i in 0..($cuentas.Length - 1)) {
+        $linea = $cuentas[$i]
+        $email = $linea.Split(":")[0]
+        $password = $linea.Split(":")[1]
+    }
+    
+    ## Obtener datos
+    ##$email = $cuentas[$i][0]
+    ##$password = $cuentas[$i][1]
 
     # Abre la página web
-    Start-Process "https://www.netflix.com/es/Login"
-
+    Start-Process -WindowStyle Hidden "chrome.exe" "https://www.netflix.com/es/login"
     # Esperar a que cargue
-    $pageLoaded = (Get-Process "explorer" | Where-Object { $_.MainWindowTitle -eq "Netflix" }).MainWindowTitle
-    while ($pageLoaded -ne "Netflix") {
-        Start-Sleep 1
-        $pageLoaded = (Get-Process "explorer" | Where-Object { $_.MainWindowTitle -eq "Netflix" }).MainWindowTitle
-    }
+
+    Start-Sleep 3
 
     # Ingresa el correo electrónico
-    $emailField = $page.FindElementById("email")
+    $emailField = $page.FindElementByXPath('//*[@id="id_userLoginId"]')
     $emailField.SendKeys($email)
 
     # Ingresa la contraseña
-    $passwordField = $page.FindElementById("password")
+    $passwordField = $page.FindElementByXPath('//*[@id="id_password"]')
     $passwordField.SendKeys($password)
 
     # Hace clic en el botón "Iniciar sesión"
-    $loginButton = $page.FindElementById("login-button")
+    $loginButton = $page.FindElementByXPath("//*[@id='appMountPoint']/div/div[3]/div/div/div[1]/form/button")
     $loginButton.Click()
 
     # Verifica si el inicio de sesión fue exitoso
     $isLoggedIn = (Get-Process "explorer" | Where-Object { $_.MainWindowTitle -eq "Netflix - Mi cuenta" }).MainWindowTitle
     if ($isLoggedIn) {
         # Inicio de sesión exitoso
+
         break
     }
 }
